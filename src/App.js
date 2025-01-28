c2bbd92868c3d39903bc92d7927f98c4cfe064c0
Oghenetega16 <sukuruoghenetega@gmail.com>
2025-01-28 22:24:24 +0100

    In Progress

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Entry from './components/Entry';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <Entry/>
+        <Entry/>
+      </main>
+    </>
+  )
+}
+
