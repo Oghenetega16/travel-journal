fb4c89a36f3a8b1b385b158179375cdfe9537ec1
Oghenetega16 <sukuruoghenetega@gmail.com>
2025-01-29 19:25:54 +0100

    Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Entry from './components/Entry';
 import Header from './components/Header';
+import travelData from './data';
 
 export default function App() {
+  const travelDetails = travelData.map((data) => {
+    return <Entry 
+      key={data.id}
+      {...data}
+    />
+  })
   return (
     <>
       <Header />
       <main className="container">
-        <Entry/>
-        <Entry/>
+        {travelDetails}
       </main>
     </>
   )
